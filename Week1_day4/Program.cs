0070f49d623cc227eace25e6176d847c459f8ef7
Jarryd Huntley <Jarryd Huntley>
2017-04-03 10:43:44 -0400

    initial commit.

---
+++ Week1_day4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1_day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string favoriteMovie = "My favorite movie is Lord of the Rings";
+            Console.WriteLine(favoriteMovie.IndexOf("movie"));
+
+            
+
+
+
+            //string firstPart = "Hello! My name is ";
+            //string secondPart = "Jarryd.";
+            //string greeting = string.Concat(firstPart, secondPart);
+
+            //Console.WriteLine(greeting);
+            //// do some stuff
+            //// do something else
+            ////do some more stuff
+            ////print out our message again
+            //Console.WriteLine(greeting);
+
+
+            //string name1 = "Jarryd";
+            //string name2 = "Jarryd";
+
+            //if(name1.Equals(name2))
+            //{
+            //    Console.WriteLine("The names are equal");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The names are not equal");
+            //}
+        }
+    }
+}
